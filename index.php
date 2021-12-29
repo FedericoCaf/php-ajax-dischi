b1c7a06b7e6e9671a8cefa14188ad715168108d6
FedericoCaf <fedetheshield09@gmail.com>
2021-12-29 12:28:49 +0100

    milestone-1-ok

--- index.php
+++ index.php
@@ -2,6 +2,82 @@
 
 $name = 'pippo';
 
+$dischi = [
+  [
+    'poster' => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+    'title' => 'New Jersey',
+    'author' => 'Bon Jovi',
+    'genre' => 'Rock',
+    'year' => 1988
+  ],
+  [
+    'poster' => "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+    'title' => 'Live at Wembley 86',
+    'author' => 'Queen',
+    'genre' => 'Pop',
+    'year' => 1992
+  ],
+  [
+    'poster' => "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+    'title' => "Ten's Summoner's Tales",
+    'author' => 'Sting',
+    'genre' => 'Pop',
+    'year' => 1993
+    ],
+  [
+    'poster' => "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+    'title' => 'Steve Gadd Band',
+    'author' => 'Steve Gadd Band',
+    'genre' => 'Jazz',
+    'year' => 2018
+],
+  [
+    'poster' => "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+    'title' => 'Brave new World',
+    'author' => 'Iron Maiden',
+    'genre' => 'Metal',
+    'year' => 2000
+],
+  [
+    'poster' => "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+    'title' => 'One more car, one more raider',
+    'author' => 'Eric Clapton',
+    'genre' => 'Rock',
+    'year' => 2002
+  ],
+  [
+    'poster' => "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+    'title' => 'Made in Japan',
+    'author' => 'Deep Purple',
+    'genre' => 'Rock',
+    'year' => 1972
+  ],
+  [
+    'poster' => "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+    'title' => 'And Justice for All',
+    'author' => 'Metallica',
+    'genre' => 'Metal',
+    'year' => 1988
+  ],
+  [
+    'poster' => 	"https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+    'title' => 'Hard Wired',
+    'author' => 'Dave Weckl',
+    'genre' => 'Jazz',
+    'year' => 1994
+  ],
+  [
+    'poster' => "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+    'title' => 'Bad',
+    'author' => 'Michael Jackson',
+    'genre' => 'Pop',
+    'year' => 1987
+  ],
+
+
+
+]
+
 ?>
 
 <!DOCTYPE html>
@@ -10,18 +86,42 @@ $name = 'pippo';
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-1/css/all.min.css" integrity="sha512-wDB6AYiYP4FO5Sxieamqy9wtpAY3qdHMqlhZecIEUu1YjkLw5gQf/4ZDgOzmCBAF5SheMjmugkpUSVoUrGbLkQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="css/style.css">
   <title>Dischi PHP</title>
 </head>
 <body>
   
   <div id="app" >
+    <header>
+      
+    </header>
+    <div class="fc-container">
+       <?php 
+        //  var_dump($dischi)  
+        for($i=0; $i<count($dischi); $i++) {  ?>
+         <div class="card-container">
+              <div class="img-container">
+                 <img src=
+                 <?php echo $dischi[$i]['poster']?> alt="poster">
+               </div> 
+    
+              <div class="text-container">
+                  <h3> <?php echo $dischi[$i]['title'] ?> </h3>
+                  <p> <?php echo $dischi[$i]['author'] ?></p>
+                  <p> <?php echo $dischi[$i]['genre'] ?></p>
+              </div>
+        </div>
+      <?php } ?>
+       
 
-    <h1> <?php echo $name ?> </h1>
+    </div>
+
 
   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-  <script src="script.js"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script> -->
+  <!-- <script src="js/script.js"></script> -->
 </body>
 </html>
